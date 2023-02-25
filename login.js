691b8e2c58f02cf897cc6afe7e372a4224d414c8
Mick04 <mac5y4@talktalk.net>
2023-02-25 14:09:43 +0000

    removed all unwanted comments

--- login.js
+++ login.js
@@ -1,18 +1,12 @@
-
-  let storedUsername = "Donald"
-  let storedPassword = "Duck"
-
-console.log('*****')
-
-function checkInput(){
-  let username = document.getElementById('username').value
-  let password = document.getElementById('password').value
+let storedUsername = "Donald";
+let storedPassword = "Duck";
+function checkInput() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
   if (username == storedUsername && password == storedPassword) {
-    document.location.href= "settings.html"
-  }
-  else {
+    document.location.href = "settings.html";
+  } else {
     alert("Either Username or Password is incorrect");
   }
-
 }
-// 
+//
